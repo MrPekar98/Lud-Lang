310939c9d510b246af65210d34f2ef06c3b25b08
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-04-05 20:22:32 +0200

    Added field to represent scope level.

--- src/lexer.h
+++ src/lexer.h
@@ -10,5 +10,5 @@ typedef struct
 // Stream representing a whole read line of tokens.
 typedef struct
 {
-
+    unsigned short level;       // Scope level found by reading amount of spaces before the first character in a line.
 } token_line;
