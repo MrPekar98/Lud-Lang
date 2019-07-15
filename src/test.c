d94ef70a9343393a40d585e16c4c2055c1b5c685
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-07-15 11:16:21 -0700

    New test of class information.

--- src/test.c
+++ src/test.c
@@ -10,6 +10,7 @@ void test_table_element();
 void test_table_insert();
 void test_get_function();
 void test_function_scope();
+void test_get_class();
 
 // Main function.
 int main()
@@ -20,6 +21,7 @@ int main()
     test_table_insert();
     test_get_function();
     test_function_scope();
+    test_get_class();
 
     return 0;
 }
@@ -66,7 +68,7 @@ void test_get_function()
 {
     struct variable var1 = {.name = "arg1", .data_type = NUM}, var2 = {.name = "arg2", .data_type = STRING};
     struct variable params[] = {var1, var2};
-    struct function func = get_function(VOID, "test_function", params, 2);
+    struct function func = get_function(NONE, 0, VOID, "test_function", params, 2);
 
     assert(strcmp(func.name, "test_function") == 0);
     assert(func.return_type == VOID);
@@ -79,7 +81,7 @@ void test_get_function()
 void test_function_scope()
 {
     struct variable element = {.name = "Some element", .data_type = BOOL};
-    struct function func = get_function(VOID, "test_function", NULL, 0);
+    struct function func = get_function(NONE, 0, VOID, "test_function", NULL, 0);
     struct table_element te = {.type = VARIABLE, .element = &element};
 
     table_insert(&func.table, te, sizeof(te), ++line);
@@ -89,4 +91,23 @@ void test_function_scope()
     assert(!func.table.open);
     assert(func.table.count == 1);
     assert(sizeof(func.table.elements[0]) == sizeof(te));
+}
+
+// Test information of get_class.
+void test_get_class()
+{
+    struct variable var1 = get_variable(NONE, 0, 0, NUM, "var1", 0, NULL);
+    struct variable var2 = get_variable(NONE, 0, 0, NUM, "var2", 0, NULL);
+    struct variable vars[] = {var1, var2};
+    struct function fun1 = get_function(NONE, 0, BOOL, "fun1", vars, 2);
+    struct function fun2 = get_function(NONE, 0, NUM, "fun2", NULL, 0);
+    struct function functions[] = {fun1, fun2};
+    struct class cl = get_class("MyClass", vars, 2, functions, 2);
+
+    assert(cl.open);
+    assert(strcmp(cl.name, "MyClass") == 0);
+    assert(cl.variable_amount == 2);
+    assert(cl.function_amount == 2);
+    assert(strcmp(cl.variables[1].name, var2.name) == 0);
+    assert(strcmp(cl.functions[0].name, fun1.name) == 0);
 }
