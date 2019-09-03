3b8a4913fb40d758e5c597b3da02b285bb6186c3
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-09-03 17:35:42 +0200

    New parser file.

---
+++ src/parser.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "token.h"
+
+// External prototype.
+extern lex_t read_token();
+
+// External line number variable.
+extern unsigned long line;
+
+// Prototypes.
+
+// Main parsing function.
+node parse()
+{
+
+}
