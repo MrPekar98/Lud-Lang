1cc49a8ad25abaed0957f99449be67cfc8821812
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-09-03 14:26:13 +0200

    Added error code.

--- src/token.h
+++ src/token.h
@@ -2,12 +2,13 @@
 
 // Enum for all tokens.
 enum token_t {LPARAN, RPARAN, LBRACE, RBRACE, IMPORT, ID, LITERAL, PROTOCOL, CLASS, ARROW, RBRACKET, LBRACKET, USING, COLON, SEMICOLON, SYSTEM, ACCESSOR,
-                STATIC, CONST, DATATYPE, VOID, EQUALS, DOT, COMMA, THIS, SUPER, CREATE, OR, AND, XOR, OPERATOR, MODULUS, IF, ELSE, FOR, WHILE, FOREACH
-                METHOD, CONSTRUCTOR};
+                STATIC, CONST, DATATYPE, VOID, EQUALS, DOT, COMMA, THIS, SUPER, CREATE, OR, AND, XOR, OPERATOR, MODULUS, IF, ELSE, FOR, WHILE, FOREACH,
+                METHOD, CONSTRUCTOR, RETURN};
 
-// Token structure.
+// Token structure. An error occured when token is -1.
 typedef struct
 {
     char lexeme[LEX_LENGTH];
     enum token_t token;
+    int error;
 } lex_t;
