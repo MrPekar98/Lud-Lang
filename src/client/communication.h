df19408bcb754f7383d2856cd490831354965b0f
MrPekar98 <martin.pekar@outlook.dk>
2019-10-21 22:37:56 +0200

    Added function to convert struct in string.

--- src/client/communication.h
+++ src/client/communication.h
@@ -1,9 +1,16 @@
 // Struct for sending file to compiler server.
+// TODO: Add description of fields.
 typedef struct 
 {
     int url_specified;
     char *url;
     int is_renamed;
     char *name;
-    int is_offline;
+    int is_offline;	// Tells whether to send package. Might not be needed.
 } package;
+
+// Converts struct into string format.
+char *package_string(package p)
+{
+    return NULL;
+}
