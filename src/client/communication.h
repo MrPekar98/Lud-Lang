f0dc72099b06c4d4348584022c9a92a5de53f7bb
Martin Pekár <32843490+MrPekar98@users.noreply.github.com>
2019-12-19 11:15:03 +0100

    Added describing comments.

--- src/client/communication.h
+++ src/client/communication.h
@@ -1,16 +1,9 @@
-// Struct for sending file to compiler server.
-// TODO: Add description of fields.
+// Communication package struct for sending file to compiler server.
 typedef struct 
 {
-    int url_specified;
-    char *url;
-    int is_renamed;
-    char *name;
-    int is_offline;	// Tells whether to send package. Might not be needed.
+    int url_specified;      // Tells whether input file has been specified.
+    char *url;              // Name of input file.
+    int is_renamed;         // Tells whether option '-o' has been used to set name of file.
+    char *name;             // Name of output file.
+    int is_offline;         // Tells whether compilation happens offline.
 } package;
-
-// Converts struct into string format.
-char *package_string(package p)
-{
-    return NULL;
-}
