2ec6c77750427defadb022e1e6bb0e98ccd9c827
MrPekar98 <martin.pekar@outlook.dk>
2019-12-23 18:05:53 +0100

    First version of header.

--- src/client/communication.h
+++ src/client/communication.h
@@ -2,6 +2,7 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 // Communication package struct for sending file to compiler server.
 typedef struct 
@@ -13,27 +14,30 @@ typedef struct
     int is_offline;         // Tells whether compilation happens offline.
     int just_compile;       // Tells whether to return target code or executable.
     char *source;           // All text to be compiled.
+    int update;             // Request to get updated executable.
 } package;
 
 // Returns buffer of package.
-// Format: First 8 bytes tell size of buffer, second 4 bytes tell size of name, third byte tells whether output file is renamed, fourth comes name of output as a string, fifth byte tells what to output (just compile), and sixth is source code as a string.
-// TODO: Make sure at least one byte is send as name. The same goes for the source code.
+// Format: First 8 bytes tell size of buffer, second 4 bytes tell size of name, third byte tells request for update,
+// fourth byte tells whether output file is renamed, fifth comes name of output as a string, sixth byte tells what to output (just compile),
+// and seventh is source code as a string.
 void *tobuffer(package p)
 {
     if (!p.url_specified)
         return NULL;
 
     void *buffer = malloc(strlen(p.url) + strlen(p.name) + 2);
-    char renamed = p.is_renamed ? (char) 1 : (char) 0, just_compile = p.just_compile ? (char) 1 : (char) 0;
-    long buffer_size = strlen(p.name) + strlen(p.source) + 10;
+    char renamed = p.is_renamed ? (char) 1 : (char) 0, just_compile = p.just_compile ? (char) 1 : (char) 0, update = p.update;
+    long buffer_size = strlen(p.name) + strlen(p.source) + 15;
     int name_size = strlen(p.name);
 
     memcpy(buffer, &buffer_size, 8);
-    memcpy(buffer + 8, name_size, 4);
-    memcpy(buffer + 12, &renamed, 1);
-    memcpy(buffer + 13, p.name, strlen(p.name));
-    memcpy(buffer + strlen(p.name) + 13, just_compile, 1);
-    memcpy(buffer + strlen(p.name) + 14, p.source, strlen(p.source));
+    memcpy(buffer + 8, &name_size, 4);
+    memcpy(buffer + 12, &update, 1);
+    memcpy(buffer + 13, &renamed, 1);
+    memcpy(buffer + 14, p.name, strlen(p.name));
+    memcpy(buffer + strlen(p.name) + 14, &just_compile, 1);
+    memcpy(buffer + strlen(p.name) + 15, p.source, strlen(p.source));
 
     return buffer;
 }
@@ -44,17 +48,19 @@ package topackage(void *buffer)
     package p;
     long buffer_size = *((long *) buffer);
     int name_size = *((int *) buffer + 8);
-    char renamed = *((char *) buffer + 12), just_compile = *((char *) buffer + 12 + name_size);
+    char renamed = *((char *) buffer + 13);
 
     if ((int) renamed)
     {
-        p.is_renamed;
+        p.is_renamed = 1;
         p.name = (char *) malloc(name_size);
         memcpy(p.name, buffer + 12, name_size);
     }
 
-    if ((int) just_compile)
-        p.just_compile = 1;
+    p.update = *((char *) buffer + 12);
+    p.just_compile = *((char *) buffer + 14 + name_size);
+    p.source = (char *) malloc(buffer_size - name_size - 15);
+    sprintf(p.source, "%s", (char *) (buffer + 15 + name_size));
 
-    // TODO: Continue from copying source code data.
+    return p;
 }
