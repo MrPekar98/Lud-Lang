844344d7adc7712421b966f7ab8a0bfc4555e7bf
MrPekar98 <martin.pekar@outlook.dk>
2020-02-05 21:49:34 +0100

    Removed token function.

--- src/parser.c
+++ src/parser.c
@@ -33,19 +33,6 @@ node parse()
     return start;
 }
 
-// Adds child to parent.
-static inline void add_child(node *parent, node child)
-{
-    if (parent->children == 0)
-        parent->children = (void **) malloc(sizeof(node));
-
-    else
-        parent->children = (void **) realloc(parent->children, sizeof(node) * (parent->children_count + 1));
-
-    parent->children[parent->children_count] = &child;
-    parent->children_count++;
-}
-
 // Makes node for IMPORT.
 static void make_import(node *parent)
 {
