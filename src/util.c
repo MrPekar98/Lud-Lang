c85d4a607373bb83b58ef48561e760f1fb7cded9
MrPekar98 <martin.pekar@outlook.dk>
2020-11-25 18:10:53 +0100

    For compilation purposes.

---
+++ src/util.c
@@ -0,0 +1,17 @@
+#include "util.h"
+#include <stdlib.h>
+
+extern unsigned long line;
+
+// Throws error.
+void error(const char *msg)
+{
+    printf("Error on line %d: %s\n", line, msg);
+    exit(1);
+}
+
+// Throws warning.
+void warning(const char *msg)
+{
+    printf("Warning on line %d: %s\n", line, msg);
+}
--- src/util.h
+++ src/util.h
@@ -3,19 +3,7 @@
 
 #include <stdio.h>
 
-extern unsigned long line;
-
-// Throws error.
-void error(const char *msg)
-{
-    printf("Error on line %d: %s\n", line, msg);
-    exit(1);
-}
-
-// Throws warning.
-void warning(const char *msg)
-{
-    printf("Warning on line %d: %s\n", line, msg);
-}
+void error(const char *msg);
+void warning(const char *msg);
 
 #endif
